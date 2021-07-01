3529d179e94c406e4fa2753e9cf78b54b2aba27e
roblesok <roblesok>
2021-06-30 22:35:36 -0300

    fix exec command

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -55,9 +55,9 @@ return require("packer").startup(function()
       vim.api.nvim_exec([[
         let g:vim_markdown_folding_disabled = 1
         let g:vim_markdown_conceal = 1
-        let g:tex_conceal = ""
+        let g:tex_conceal = ''
         let g:vim_markdown_math = 1
-      ]])
+      ]], false)
     end
   }
 
